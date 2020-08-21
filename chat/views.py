105cc89d2b7f4e2e64e83835db992588dd40e1dc
Elle <codingwithelle@gmail.com>
2020-08-21 20:45:38 +0800

    Fine tuning

--- chat/views.py
+++ chat/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 # chat/views.py
 from django.shortcuts import render
 
+@login_required
 def index(request):
     return render(request, 'chat/index.html', {})
 
+@login_required
 def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
