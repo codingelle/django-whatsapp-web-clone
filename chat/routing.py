addae17b13d9e3bdc19335d130c81d8a0f98cc31
josnin <nino.capati@gmail.com>
2021-04-29 17:47:23 +0800

    django channels upgrade from 2 to 3 (#11)
    * django channels upgrade from 2 to 3
    
    * channel-redis also required to upgrade from 3.1 -> 3.2

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
