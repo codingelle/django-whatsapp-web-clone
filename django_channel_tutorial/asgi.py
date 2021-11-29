1f0bf6234705dfdb39748771ebd894a7b694a58c
josnin <nino.capati@gmail.com>
2021-11-29 19:49:42 +0800

    Josnin patch 3 (#21)
    * [fix] Django can only handle ASGI/HTTP connections, not websocket
    
    * removed console

--- django_channel_tutorial/asgi.py
+++ django_channel_tutorial/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
+import django
 
-from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channel_tutorial.settings')
 
-application = get_asgi_application()
+django.setup()
+
+application = get_default_application()
